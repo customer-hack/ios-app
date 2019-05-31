015bf8e2aea697d8e1486f3e11cc8fd716166a83
Justin Hoyt <justinhoyt24@gmail.com>
2019-05-31 12:56:42 -0400

    Add image switching on TDK

--- 3M-PoC/ProxyManager.swift
+++ 3M-PoC/ProxyManager.swift
@@ -37,54 +37,53 @@ class ProxyManager: NSObject {
         let darkGrey = SDLRGBColor(red: 57, green: 78, blue: 96)
         lifecycleConfiguration.dayColorScheme = SDLTemplateColorScheme(primaryRGBColor: green, secondaryRGBColor: grey, backgroundRGBColor: white)
         lifecycleConfiguration.nightColorScheme = SDLTemplateColorScheme(primaryRGBColor: green, secondaryRGBColor: grey, backgroundRGBColor: darkGrey)
-        
+
         if let appImage = UIImage(named: "iconLogo") {
             let appIcon = SDLArtwork(image: appImage, name: "fordicon.jpg", persistent: true, as: .JPG)
             lifecycleConfiguration.appIcon = appIcon
         }
-        
+
         let configuration = SDLConfiguration(lifecycle: lifecycleConfiguration, lockScreen: .disabled(), logging: .default(), fileManager: .default())
-        
+
         sdlManager = SDLManager(configuration: configuration, delegate: self as? SDLManagerDelegate)
 
     }
-    
+
     func connect() {
         // Start watching for a connection with a SDL Core
         sdlManager.start { (success, error) in
             if success {
                 // Your app has successfully connected with the SDL Core
-                self.updateScreen()
-                let display = SDLSetDisplayLayout(predefinedLayout: .graphicWithText)
-                self.sdlManager.send(request: display) { (request, response, error) in
-                    if response?.resultCode == .success {
-                        // The template has been set successfully
-                    }
+                if let appImage = UIImage(named: "JoeLouis.jpg") {
+                    self.updateScreen(image: appImage, text1: "Joe", text2: "Louis", template: .graphicWithText)
                 }
             }
         }
     }
-    
-    func updateScreen() {
+
+    func updateScreen(image: UIImage, text1: String, text2: String, template: SDLPredefinedLayout) {
         sdlManager.screenManager.beginUpdates()
-        if let appImage = UIImage(named: "JoeLouis.jpg") {
-            let joeLouisArtwork = SDLArtwork(image: appImage, persistent: true, as: .JPG)
-            sdlManager.fileManager.upload(artwork: joeLouisArtwork) {
-                (success, artworkName, bytesAvailable, error) in
-                guard error == nil else {return}
-            }
-            sdlManager.screenManager.primaryGraphic = joeLouisArtwork
+
+        let artwork = SDLArtwork(image: image, persistent: false, as: .JPG)
+        self.sdlManager.fileManager.upload(artwork: artwork) {
+            (success, artworkName, bytesAvailable, error) in
+            guard error == nil else { return }
+            self.sdlManager.screenManager.primaryGraphic = artwork
         }
-        
-        
-        sdlManager.screenManager.textField1 = "hello"
-        sdlManager.screenManager.textField2 = "world"
+
+        sdlManager.screenManager.textField1 = text1
+        sdlManager.screenManager.textField2 = text2
 
         sdlManager.screenManager.endUpdates { (error) in
             if error != nil {
                 print("Error in sdlManager.screenManager.endUpdates")
             }
         }
+        let display = SDLSetDisplayLayout(predefinedLayout: template)
+        self.sdlManager.send(request: display) { (request, response, error) in
+            if response?.resultCode == .success {
+                // The template has been set successfully
+            }
+        }
     }
 }
-
--- 3M-PoC/ViewController.swift
+++ 3M-PoC/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let proxyManager = ProxyManager.self
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showChimera(_ sender: Any) {
+        if let detroitChimeraImage = UIImage(named: "DetroitChimera.jpg") {
+            ProxyManager.sharedManager.updateScreen(image: detroitChimeraImage, text1: "Detroit", text2: "Chimera", template: .graphicWithText)
+        }
+    }
+    @IBAction func showLargeGraphic(_ sender: Any) {
+        if let detroitChimeraImage = UIImage(named: "DetroitChimera.jpg") {
+            ProxyManager.sharedManager.updateScreen(image: detroitChimeraImage, text1: "Detroit", text2: "Chimera", template: .graphicWithText)
+        }
+    }
+    
 }
 
